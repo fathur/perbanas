afbbd78c22b8755bdba369f4b44a90b57ecaa57f
fathur <fathur_rohman17@yahoo.co.id>
2014-06-04 13:26:37 +0700

    memberbank

--- wp-content/themes/perbanas/single-memberbank.php
+++ wp-content/themes/perbanas/single-memberbank.php
@@ -1,25 +1,87 @@
 <?php get_header(); ?>
-<div class="row">
-	<div class="col-md-4">
-		<?php // echo get_post_type(); 
-		
-		wp_nav_menu( array(
-			'theme_location'  => get_post_type() . '-menu'
-		) );
-		?>
-	</div>
-	<div class="col-md-8">
-		<?php 
+<div class="container main-layout member-banks-detail">
+    <div class="row breadcrumbs">
+        <div class="col-xs-12">
+            <a href="#" class="elem">Home</a> > <a href="#" class="elem">Council</a> > <a href="#" class="elem">Central Board</a> > <a href="#" class="elem">Sectors</a> > <a href="#" class="elem">Organizational Sector</a>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-xs-12 col-sm-3 sidebar">
+            <div class="sidebar-title">
+                <h2>Member Banks</h2>
+            </div>
+            
+            <?php echo perbanas_side_menu('member-bank-menu', 'leftMenu'); ?>
+        </div>
+        <div class="col-xs-12 col-sm-9 main-content-pane">
+        
+        <?php 
 		while ( have_posts() ) : the_post();
 		
-			get_template_part( 'content', 'member' );
 			
-			if ( comments_open() || get_comments_number() ) {
-				comments_template();
-			}
+			
+		?>
+		
+            <div class="row">
+                <div class="col-xs-12 section-title">
+                    <h1><span>
+                    <?php 
+                    if ( is_single() ) :
+                    the_title();
+                    else :
+                    the_title( '<a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a>' );
+                    endif;
+                    ?>
+                    </span></h1>
+                </div>
+            </div>
+            <div class="row main-content-pane-logo">
+                <div class="col-xs-12">
+					<?php the_post_thumbnail('',array('class' => "img-no-responsive",)); ?>
+				</div>
+            </div>
+            <div class="row">
+                <div class="col-sm-12 col-md-3 main-content-pane-left-column">
+                    <div class="row">
+                        <div class="col-xs-12">
+                            <h2><?php echo get_post_meta( get_the_ID(), 'wpcf-memberbank-title', TRUE ); ?></h2>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-xs-6 col-sm-12">
+                            <p>
+                                <span class="main-content-pane-left-column-paragraph-heading hidden-md hidden-lg">Address:</span><br class="hidden-md hidden-lg" />
+								<?php echo get_post_meta( get_the_ID(), 'wpcf-memberbank-address', TRUE ); ?>
+                            </p>
+                        </div>
+                        <div class="col-xs-6 col-sm-12">
+                            <p>
+                                <span class="main-content-pane-left-column-paragraph-heading">Telp:</span><br />
+                                <?php echo get_post_meta( get_the_ID(), 'wpcf-memberbank-phones', TRUE ); ?>
+                            </p>
+                        </div>
+                        <div class="col-xs-12 col-sm-12">
+                            <p>
+                                <a href="<?php echo get_post_meta( get_the_ID(), 'wpcf-memberbank-website', TRUE ); ?>"><?php echo get_post_meta( get_the_ID(), 'wpcf-memberbank-website', TRUE ); ?></a>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-12 col-md-9 main-content-pane-right-column">
+                    <?php the_content(); ?>
+                </div>
+            </div>
+            
+            <?php 
+	
+			
+			
 			
 		endwhile;		
 		?>
-	</div>
+        </div>
+    </div>
 </div>
+        
+
 <?php get_footer(); ?>
--- wp-content/themes/perbanas/taxonomy-submemberbank.php
+++ wp-content/themes/perbanas/taxonomy-submemberbank.php
@@ -5,53 +5,59 @@ $menu_post_type = 'member-bank'; // Karena sector merupakan bagian dari menu cou
 
 get_header(); ?>
 
-<!-- http://localhost/perbanas/subsector/law-sector/
 
-menampilkan halaman per taxonomy sector
-yang ditampilkan adalah nama orangnya -->
-
-<div class="row">
-    <div class="col-md-4">
-        <?php // echo get_post_type(); 
-        
-        wp_nav_menu( array(
-            'theme_location'  => $menu_post_type . '-menu'
-        ) );
-        ?>
+<div class="container main-layout regional-board">
+    <div class="row breadcrumbs">
+        <div class="col-xs-12">
+            <a href="#" class="elem">Home</a> > <a href="#" class="elem">Council</a> > <a href="#" class="elem">Central Board</a> > <a href="#" class="elem">Sectors</a> > <a href="#" class="elem">Organizational Sector</a>
+        </div>
     </div>
-    <div class="col-md-8">
-    <?php 
+    <div class="row">
+        <div class="col-xs-12 col-sm-3 sidebar">
+            <div class="sidebar-title">
+                <h2>Member Banks</h2>
+            </div>
+            
+            <?php echo perbanas_side_menu('member-bank-menu', 'leftMenu'); ?>
+        </div>
+        <div class="col-xs-12 col-sm-9 main-content-pane">
+            <div class="row">
+                <div class="col-xs-12 section-title">
+                    <h1><span>Regional Board</span></h1>
+                </div>
+            </div>
+           <div class="row">
+				<?php
+	           
+				wp_reset_query();
+	           
+				$args = array('post_type' => get_post_type(),
+					'tax_query' => array(
+						array(
+							'taxonomy'  => $taxonomy,
+							'field'     => 'slug',
+							'terms'     => $term
+					)));
 
-        wp_reset_query();
-
-        $args = array('post_type' => get_post_type(),
-                'tax_query' => array(
-                    array(
-                        'taxonomy'  => $taxonomy,
-                        'field'     => 'slug',
-                        'terms'     => $term,
-                    ),
-                ),
-            );
-
-        $loop = new WP_Query($args);
-
-        if( $loop->have_posts() ) : 
-        ?>
-
-        <ul>
-        <?php while($loop->have_posts()) : $loop->the_post(); ?>
-            <li>
-                <a href="<?php echo get_permalink( get_the_ID() ); ?>" title="<?php echo esc_attr( get_the_title() ); ?>">
-                    <?php echo get_the_post_thumbnail(); ?>
-                </a>
-            </li>
-        <?php endwhile; ?>
-        </ul>
-
-        <?php
-        endif;
-    ?> 
+				$loop = new WP_Query($args);
+				
+				if( $loop->have_posts() ) : 
+					while($loop->have_posts()) : $loop->the_post();
+				?>
+				
+				<div class="col-xs-6 col-sm-3">
+					<a href="<?php echo get_permalink( get_the_ID() ); ?>" title="<?php echo esc_attr( get_the_title() ); ?>">
+						<?php echo get_the_post_thumbnail(); ?>
+					</a>
+                </div>
+				
+				<?php 
+					endwhile; 
+				endif;
+				
+				?> 
+            </div>
+        </div>
     </div>
 </div>
 
