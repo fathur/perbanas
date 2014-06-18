d86344b88455dbf96e9bdb1549e313897427c924
lolski <soulmagic13@gmail.com>
2014-06-18 11:44:17 +0700

    testing why affix doesn't work on council page

--- wp-content/themes/perbanas/js/functions.js
+++ wp-content/themes/perbanas/js/functions.js
@@ -97,16 +97,11 @@ function adjustNavbarMenu(expand, paddingNarrow, paddingOriginal) {
         }
     );
 }
-$.fn.log = function() {
-  console.log.apply(console, this);
-  return this;
-};
 function sidebarAffix() {
     $('.main-content-pane').css('height', 864);
     var mainContentPaneHeight = $('.main-content-pane').outerHeight();
     if (mainContentPaneHeight > 700) {
-        //var offset = $('.website-header').outerHeight(true) + $('.navbar-container').outerHeight(true) + $('.breadcrumbs').outerHeight(true);
-        $('.sidebar').log();
+        console.log($('.sidebar')[0]);
         $('.sidebar').affix({'offset': {'top': 301, 'bottom': 0}});
     }
 }
