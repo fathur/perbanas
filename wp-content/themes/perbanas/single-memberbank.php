b4c76d313aa6dcd51ba60f5ad2e768c707c7b9b4
fathur <fathur_rohman17@yahoo.co.id>
2014-06-18 14:48:43 +0700

    memberbank layout update

--- wp-content/themes/perbanas/single-memberbank.php
+++ wp-content/themes/perbanas/single-memberbank.php
@@ -1,5 +1,5 @@
 <?php get_header(); ?>
-<div class="container main-layout member-banks-detail">
+<div class="container main-layout regional-board-inside">
     <div class="row breadcrumbs hidden-xs hidden-sm">
         <div class="col-xs-12">
 			<?php if(function_exists('bcn_display')) { bcn_display(); }?>
@@ -16,71 +16,51 @@
             </div>
         </nav>
         <div class="col-xs-12 col-md-9 main-content-pane">
-        
-        <?php 
-		while ( have_posts() ) : the_post();
-		
-			
-			
-		?>
-		
+            <div class="row">
+                <div class="col-xs-12 text-right">
+                    <select>
+                    	<option>Other Regional Board</option>
+                        <?php 
+							$args = array( 'post_type' => get_post_type() );
+							$query = new WP_Query( $args );
+						
+							if ( $query->have_posts() ) {
+								
+								while ( $query->have_posts() ) {
+									$query->the_post(); ?>
+									
+									<option value="<?php echo basename(get_permalink()); ?>"><?php echo get_the_title(); ?></option>
+									<?php 
+								}
+								
+							} 
+							wp_reset_postdata();
+						
+						?> 
+                    </select>
+                </div>
+            </div>
+            <?php while ( have_posts() ) : the_post(); ?>
             <div class="row">
                 <div class="col-xs-12 section-title">
                     <h1><div></div><span>
                     <?php 
+                  
                     if ( is_single() ) :
-                    the_title();
-                    else :
-                    the_title( '<a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a>' );
-                    endif;
-                    ?>
+						the_title();
+					else :
+						the_title( '<a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a>' );
+					endif;
+					?>
                     </span></h1>
                 </div>
             </div>
-            <div class="row main-content-pane-logo">
-                <div class="col-xs-12">
-					<?php the_post_thumbnail('',array('class' => "img-no-responsive",)); ?>
-				</div>
-            </div>
             <div class="row">
-                <div class="col-sm-12 col-md-3 main-content-pane-left-column">
-                    <div class="row">
-                        <div class="col-xs-12">
-                            <h2><?php echo get_post_meta( get_the_ID(), 'wpcf-memberbank-title', TRUE ); ?></h2>
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="col-xs-6 col-sm-12">
-                            <p>
-                                <span class="main-content-pane-left-column-paragraph-heading hidden-md hidden-lg">Address:</span><br class="hidden-md hidden-lg" />
-								<?php echo get_post_meta( get_the_ID(), 'wpcf-memberbank-address', TRUE ); ?>
-                            </p>
-                        </div>
-                        <div class="col-xs-6 col-sm-12">
-                            <p>
-                                <span class="main-content-pane-left-column-paragraph-heading">Telp:</span><br />
-                                <?php echo get_post_meta( get_the_ID(), 'wpcf-memberbank-phones', TRUE ); ?>
-                            </p>
-                        </div>
-                        <div class="col-xs-12 col-sm-12">
-                            <p>
-                                <a href="<?php echo get_post_meta( get_the_ID(), 'wpcf-memberbank-website', TRUE ); ?>"><?php echo get_post_meta( get_the_ID(), 'wpcf-memberbank-website', TRUE ); ?></a>
-                            </p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-sm-12 col-md-9 main-content-pane-right-column">
-                    <?php the_content(); ?>
+                <div class="col-xs-12">
+					<?php the_content(); ?>
                 </div>
             </div>
-            
-            <?php 
-	
-			
-			
-			
-		endwhile;		
-		?>
+            <?php endwhile; ?>
         </div>
     </div>
 </div>
