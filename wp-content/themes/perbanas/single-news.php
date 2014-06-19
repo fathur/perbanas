039870f86cdfd2f6d5c47adcb48ad53d2467b57b
fathur <fathur_rohman17@yahoo.co.id>
2014-06-19 15:54:26 +0700

    update no picture for news and magazine

--- wp-content/themes/perbanas/single-news.php
+++ wp-content/themes/perbanas/single-news.php
@@ -1,4 +1,13 @@
-<?php get_header(); ?>
+<?php 
+
+/**
+ * Menampilkan halaman untuk satu berita saja.
+ * 
+ * @author Fostrom
+ * 
+ * */
+
+get_header(); ?>
 
 <div class="container main-layout news-single">
     <div class="row breadcrumbs hidden-xs hidden-sm">
@@ -22,29 +31,25 @@
                 </div>
             </div>
             
-            <?php 
-            while ( have_posts() ) : the_post(); 
-            $id_post = get_the_ID(); ?>
-            
+            <?php while ( have_posts() ) : the_post(); 
+				$id_post = get_the_ID(); ?>
             
             <div class="row">
                 <div class="col-xs-12">
                     <div class="top-row">
                         <p class="text-right back-link">
-                            <a href="<?php echo get_post_type_archive_link(get_post_type()); ?>"><span class="arrow">&lang;</span> Back to Perbanas News</a>
+                            <a href="<?php echo get_post_type_archive_link( get_post_type() ); ?>"><span class="arrow">&lang;</span> <?php _e('Back to Perbanas News','perbanas'); ?></a>
                         </p>
-                        <h2><?php 
-                    if ( is_single() ) :
-                    the_title();
-                    else :
-                    the_title( '<a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a>' );
-                    endif;
-                    ?></h2>
+                        <h2><?php the_title(); ?></h2>
                         <p class="date"><?php the_date(); ?></p>
                     </div>
                     <div class="news-content">
                         <p>
-                           <?php the_post_thumbnail('large',array('class' => "img-responsive",'style'=>"min-width: 100%")); ?>
+	                        <?php if (has_post_thumbnail()) {
+	                        	the_post_thumbnail('large',array('class' => "img-responsive",'style'=>"min-width: 100%;"));
+	                        } else { ?>
+								<img src="<?php echo get_template_directory_uri(); ?>/img/no-news-picture.jpg" class="img-responsive" style="min-width: 100%" />
+							<?php } ?>
                         </p>
                        <?php the_content(); ?>
                     </div>
@@ -55,49 +60,49 @@
             
             <div class="row">
                 <div class="col-xs-12 section-title">
-                    <div></div><h1><span>Other News</span></h1>
+                    <div></div><h1><span><?php _e('Other News','perbanas'); ?></span></h1>
                 </div>
             </div>
             <div class="row other-news">
             
-            <?php 
-            
-            $rp = array(
+            <?php $relateds = new WP_Query( array(
 				'post_type'			=> get_post_type(),
             	'post__not_in'		=> array($id_post),
 				'posts_per_page'	=> 4,
 				'ignore_sticky_posts' => TRUE
-            );
-            
-            $relateds = new WP_Query($rp);
+            ) );
             
            // The Loop
 			if ( $relateds->have_posts() ) {
-				
-				while ( $relateds->have_posts() ) :
-					$relateds->the_post();
-				?>
+				while ( $relateds->have_posts() ) : $relateds->the_post(); ?>
 				
 				<div class="col-xs-6 col-md-3 block">
-					<?php the_post_thumbnail('post-thumbnail',array('class'=>'img-responsive','width'=>225,'height'=>170)); ?>
+					<?php 
+					if (has_post_thumbnail()) {
+						the_post_thumbnail(array(225,170),array('class'=>'img-responsive'));
+					} else { ?>
+						<img src="<?php echo get_template_directory_uri(); ?>/img/no-news-picture.jpg" class="img-responsive" style="min-width: 100%" />
+					<?php } ?>
+					
 					<h3><?php the_title( '<a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a>' ); ?></h3>
+					
 					<p class="date"><?php the_date(); ?></p>
 				</div>
 				
-				<?php 
-					//echo get_the_title();
-				endwhile;
+			<?php endwhile;
 				
 			} else {
-				// no posts found
+				get_template_part( 'content', 'none' );
 			}
+			
+			/* Restore to originial query */
+			wp_reset_query();
 			/* Restore original Post Data */
-			wp_reset_postdata();
-            ?>
+			wp_reset_postdata(); ?>
             
-              
             </div>
         </div>
     </div>
 </div>
+
 <?php get_footer(); ?>
