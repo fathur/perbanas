ce4a8feeef32122c2b97ca69ce33efddd3fd8080
fathur <fathur_rohman17@yahoo.co.id>
2014-06-19 19:01:51 +0700

    download industry guidelines

--- wp-content/themes/perbanas/inc/breadcrumb.php
+++ wp-content/themes/perbanas/inc/breadcrumb.php
@@ -7,7 +7,7 @@
 function perbanas_breadcrumb_council( $trail ) {
 	if (get_post_type() == 'sector') {
 		//array_pop($trail->trail);
-		print_r($trail);
+		//print_r($trail);
 	}
 }
 add_action('bcn_after_fill','perbanas_breadcrumb_council');
--- wp-content/themes/perbanas/taxonomy-subindustryguidelines.php
+++ wp-content/themes/perbanas/taxonomy-subindustryguidelines.php
@@ -31,12 +31,8 @@ get_header(); ?>
                     <h1><div></div><span><?php echo get_term_by('slug', $term, $taxonomy)->name; ?></span></h1>
                 </div>
             </div>
-          
-			<?php
-           
-			wp_reset_query();
-           
-			$args = array('post_type' => get_post_type(),
+			
+			<?php $args = array('post_type' => get_post_type(),
 				'tax_query' => array(
 					array(
 						'taxonomy'  => $taxonomy,
@@ -48,12 +44,27 @@ get_header(); ?>
 			
 			if( $loop->have_posts() ) : 
 				while($loop->have_posts()) : $loop->the_post();
+			
+				/**
+				 * Mencari id file yang akan di unduh
+				 * */
+				$id_download 		= perbanas_get_metaid_by_key(get_the_ID(), 'wpcf-industryguide-attachment');
+				
+				/**
+				 * Mencari file size yang ada
+				* */
+				$size_download 		= perbanas_get_postmeta_size( perbanas_get_metaid_by_key(get_the_ID(), 'wpcf-industryguide-attachment') );
+				
+				/**
+				 * Mencari extensi file
+				* */
+				$format_download 	= perbanas_get_postmeta_extension( perbanas_get_metaid_by_key(get_the_ID(), 'wpcf-industryguide-attachment') );
 			?>
 				<div class="row industry-guidelines-syariah-business-item">
 					<div class="col-xs-10">
-						<h2><a href="<?php echo get_permalink( get_the_ID() ); ?>"><?php echo esc_attr( get_the_title() ); ?></a></h2>
+						<h2><a href="<?php echo get_post_meta( get_the_ID(), 'wpcf-industryguide-attachment', TRUE); ?>"><?php echo esc_attr( get_the_title() ); ?></a></h2>
 						
-						<p class="desc">Size: <?php echo perbanas_get_postmeta_size( perbanas_get_metaid_by_key(get_the_ID(), 'wpcf-industryguide-attachment') ); ?> <span class="divider">|</span> <?php the_date(); ?> <span class="divider">|</span> Format: <?php echo perbanas_get_postmeta_extension( perbanas_get_metaid_by_key(get_the_ID(), 'wpcf-industryguide-attachment') ); ?></p>
+						<p class="desc"><?php _e('Size','perbanas'); ?>: <?php echo $size_download; ?> <span class="divider">|</span> <?php the_date(); ?> <span class="divider">|</span> <?php _e('Size','perbanas'); ?>: <?php echo $format_download; ?></p>
 						<p class="desc-detailed">
 							<?php the_content(); ?>
 						</p>
@@ -64,11 +75,13 @@ get_header(); ?>
             </div>
             <hr />
 				
-				<?php 
-				endwhile; 
-			endif;
+			<?php endwhile;
+			else:
+				get_template_part( 'content', 'none' );
+			endif; 
 			
-				?> 
+			wp_reset_query();
+			wp_reset_postdata(); ?> 
             
         </div>
     </div>
