cb1fb2a4b930341502af50cf2be01b5e19d1b37d
fathur <fathur_rohman17@yahoo.co.id>
2014-06-19 18:44:38 +0700

    remove info.php

--- wp-content/themes/perbanas/inc/breadcrumb.php
+++ wp-content/themes/perbanas/inc/breadcrumb.php
@@ -1,10 +1,13 @@
 <?php
 
-
+/**
+ * Breadcrumb for council
+ * @param Obj $trail
+ */
 function perbanas_breadcrumb_council( $trail ) {
 	if (get_post_type() == 'sector') {
 		//array_pop($trail->trail);
-		//print_r($trail->trail);
+		print_r($trail);
 	}
 }
 add_action('bcn_after_fill','perbanas_breadcrumb_council');
