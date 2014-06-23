4ab96e0e6fed687e3cd75ed151e75d6c4c0a86bb
lolski <soulmagic13@gmail.com>
2014-06-23 15:00:05 +0700

    fixing gallery size

--- wp-content/themes/perbanas/js/page.news-and-media.js
+++ wp-content/themes/perbanas/js/page.news-and-media.js
@@ -4,7 +4,7 @@ function page_probank_magazine_init() {
 
 function gallery_photo_list_init() {
 	$('dl').removeClass();
-	$('dl').addClass('col-xs-6 col-md-3 block photo-gallery-photo-list-item');
+	$('dl').addClass('col-xs-12 col-sm-6 col-md-3 block photo-gallery-photo-list-item');
 	$('dt').removeClass();
 	$('dt').addClass('square');
 	$('dt img').addClass('img-responsive');
