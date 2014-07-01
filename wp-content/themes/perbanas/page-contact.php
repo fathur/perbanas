e161ee3a0ae838ce9719f7e3e8b4cc88658d58a1
fathur <fathur_rohman17@yahoo.co.id>
2014-07-01 13:04:20 +0700

    enble contact form

--- wp-content/themes/perbanas/page-contact.php
+++ wp-content/themes/perbanas/page-contact.php
@@ -65,7 +65,6 @@ get_header(); ?>
             </div>
         </div>
     </div>
-    <!-- 
     <div class="row section-title">
         <div class="col-xs-12 block">
             <h1>
@@ -75,10 +74,9 @@ get_header(); ?>
     </div>
     <div class="row contact-form">
     
-    	<?php // echo do_shortcode('[contact-form-7 id="302" title="Contact form En"]'); ?>
+    	<?php echo do_shortcode('[contact-form-7 id="302" title="Contact form En"]'); ?>
         
     </div>
-     -->
 </div>
 
 <?php get_footer(); ?>
